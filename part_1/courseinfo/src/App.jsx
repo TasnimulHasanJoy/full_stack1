905e8483660a8369c9baad66c2f36d4b65f58410
TasnimulHasanJoy <mdtasnimulhasanjoy@gmail.com>
2026-06-02 23:31:56 +0300

    Complete exercise 1.2

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -1,28 +1,37 @@
-const App = () => {
-  const course = 'Half Stack application development'
-
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <h1>{course}</h1>
-
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-
-      <p>
-        Number of exercises {exercises1 + exercises2 + exercises3}
-      </p>
-    </div>
-  );
+const Header = () => {
+  return <h1>Half Stack application development</h1>
 }
 
-export default App;
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part name="Fundamentals of React" exercises={10} />
+      <Part name="Using props to pass data" exercises={7} />
+      <Part name="State of a component" exercises={14} />
+    </div>
+  )
+}
+
+const Total = () => {
+  return <p>Number of exercises 31</p>
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
