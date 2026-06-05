9095d9350d58d871b502ea06cd8083061a0b1f81
TasnimulHasanJoy <mdtasnimulhasanjoy@gmail.com>
2026-06-05 16:44:56 +0300

    complete exercises 1.7

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -5,27 +5,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
 
       <h1>statistics</h1>
 
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
