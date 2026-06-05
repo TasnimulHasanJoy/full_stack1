8a5ee058dbaf90978902fc0115db9eb273b0f84a
TasnimulHasanJoy <mdtasnimulhasanjoy@gmail.com>
2026-06-05 16:58:31 +0300

    complete exercises 1.9

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -2,8 +2,18 @@ import { useState } from 'react'
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
+
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
 
   return (
     <div>
@@ -12,6 +22,7 @@ const Statistics = ({ good, neutral, bad }) => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
